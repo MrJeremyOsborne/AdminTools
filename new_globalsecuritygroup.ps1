0d8800b4a27330b808066cbad3bb41c7c41bb5d8
Jeremy Osborne <mrjeremyosborne@outlook.com>
2025-03-06 08:02:28 -0800

    Update new_globalsecuritygroup.ps1

--- new_globalsecuritygroup.ps1
+++ new_globalsecuritygroup.ps1
@@ -2,7 +2,7 @@
 $OUName     = "London"
 $DomainDN   = "DC=Adatum,DC=com"
 $OUPath     = "OU=$OUName,$DomainDN"
-$GroupName = "London Users"
+$GroupName  = "London Users"
 
 # Create a Global Security Group for "London" Users" inside the "London" OU if it does not exist
 if (-not (Get-ADGroup -Filter "Name -eq '$GroupName'" -SearchBase $OUPath)) {
